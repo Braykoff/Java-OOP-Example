166f4db4f3fcf92215d25b408954f36270c211fd
braykoff <99614905+Braykoff@users.noreply.github.com>
2023-07-10 00:26:42 -0400

    Initial commit. Created House class

---
+++ House.java
@@ -0,0 +1,24 @@
+public class House {
+    // Properties
+    private String owner;
+    private int numOfDoors;
+    private int size; // sq ft
+
+    // Methods
+    public void setDoorOpen(boolean open) {
+        if (open) {
+            System.out.printf("The doors of %s's house are open\n", owner);
+        } else {
+            System.out.printf("The doors of %s's house are closed\n", owner);
+        }
+    }
+
+    public void openWindows() {
+        System.out.printf("The windows of the house owned by %s were opened!\n", owner);
+    }
+
+    public String getString() {
+        return String.format("House owned by %s with %d doors\n", owner, numOfDoors);
+    }
+}
+
